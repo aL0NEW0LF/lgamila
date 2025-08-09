6a906e45077af7fe8f658d4449f898a4edb96379
Anas Mazouni <madadj4@gmail.com>
2025-08-09 12:29:37 +0200

    feat(web): update footer component layout and add status badge
    Closes #5

--- apps/web/src/components/molecules/footer.tsx
+++ apps/web/src/components/molecules/footer.tsx
@@ -1,16 +1,34 @@
-import { cn } from "@/lib/utils";
-import PixelHeart from "../icons/pixel-heart";
+import { cn } from '@/lib/utils';
+import PixelHeart from '../icons/pixel-heart';
 
 export default function Footer({ className }: { className?: string }) {
-  return <footer className={cn("flex flex-row items-center justify-center gap-2 uppercase py-8", className)}>
-    <span>
-      by the community
-    </span>
+  return (
+    <footer
+      className={cn(
+        'flex flex-col items-center justify-center gap-2',
+        className
+      )}
+    >
+      <iframe
+        frameBorder="0"
+        height="30"
+        scrolling="no"
+        src="https://status.lgamila.ma/badge?theme=dark"
+        style={{ colorScheme: 'normal' }}
+        width="180"
+      />
+      <div
+        className={cn(
+          'flex flex-row items-center justify-center gap-2 uppercase py-8',
+          className
+        )}
+      >
+        <span>by the community</span>
 
-    <PixelHeart />
+        <PixelHeart />
 
-    <span>
-      for the community
-    </span>
-  </footer>;
+        <span>for the community</span>
+      </div>
+    </footer>
+  );
 }
